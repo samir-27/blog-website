caf949537941177d28eef069fb82124c521e53ed
samir-27 <sumrasamir27@gmail.com>
2023-02-09 15:44:54 +0530

    add home about and compose

--- app.js
+++ app.js
@@ -18,10 +18,26 @@ app.use(express.static("public"));
 
 
 
+app.get("/",function(req,res){
+  res.render("home", {StartingContent : homeStartingContent});
+});
+
+app.get("/about",function(req,res){
+  res.render("about", {aboutContent : aboutContent});
+});
+
+app.get("/contact",function(req,res){
+  res.render("contact", {contactContent : contactContent});
+});
+
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
 
 
-
-
+app.post("/compose",function(req,res){
+  console.log(req.body.posttitle)
+});
 
 
 
